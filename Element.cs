fe949074a6d75c69287be6565a3052f62b775f4d
Buraburaite <jvrcueto@outlook.com>
2016-09-15 01:35:08 -0400

    Element transmuting system change
    Element transmuting now uses ints instead of strings. The resulting code is cleaner, shorter, and runs much faster.

--- Element.cs
+++ Element.cs
@@ -1,12 +1,13 @@
-﻿using UnityEngine;
+using UnityEngine;
 using mono = UnityEngine.MonoBehaviour;
 using System;
 using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 
-public sealed class Element : IComparable<Element>{
+public sealed class Element{
 
+	#region Instances (All)
 	public static readonly Element aether 	= new Element("Aether");
 	public static readonly Element air 		= new Element("Air");
 	public static readonly Element earth	= new Element("Earth");
@@ -43,6 +44,15 @@ public sealed class Element : IComparable<Element>{
 
 	public static readonly Element sidereal = new Element ("Sidereal");
 
+	private static readonly Element init = new Element ("Init"); //calls the Init method
+	#endregion
+
+	#region Static Variables
+	public static readonly Element[] available = new Element[] {
+		aether, air, fire, water,
+		dark, ice, light, metal, spark,
+		dragon, mech, megapoda, spirit	};
+	
 	public static readonly List<Element> elements = new List<Element> {
 		aether, air, earth, fire, water,
 		dark, ice, lava, light, metal, rain, sand, sound, spark, steam,
@@ -57,30 +67,25 @@ public sealed class Element : IComparable<Element>{
 		"Aetherless", "Airless", "Earthless", "Fireless", "Waterless",
 		"Sidereal" };
 
-	public static readonly Element[] available = new Element[] {
-		aether, air, fire, water,
-		dark, ice, light, metal, spark,
-		dragon, mech, megapoda, spirit	};
-	
+	private static Dictionary<int, Element> recipeDict;
+	#endregion
+
+	#region Non-Static Members
 	private string namef;
 	private Element[] componentsf;
+	private int recipe;
 
 	public string name {
 		get 		{ return namef; }
 		private set { namef = value; }
 	}
-
 	public Element[] components{
 		get 		{ return componentsf; }
 		private set { componentsf = value; }
 	}
+	#endregion
 
-	private static Dictionary<string, Element> recipeDict;
-
-	private string recipe;
-
-	private static readonly Element init = new Element ("Init"); //for initialization purposes
-
+	#region Methods
 	private Element(string str){
 		if (str == "Init") {
 			TextAsset eRecipesCSV = GameObject.FindObjectOfType<Transmutor> ().eRecipesCSV;
@@ -89,230 +94,85 @@ public sealed class Element : IComparable<Element>{
 			name = str;
 		}
 	}
-		
-	private static void Init(TextAsset textAsset){
-		//For ordering strings, needed a couple of times.
-		List<string> strList;
-		List<string> stringList = new List<string> ();
-		List<string> components;
-		List<char> sortingList;
 
-		//Sorting in the hopes that it might increase performance? Don't think so, but it's the same
-		elementStrings.Sort();
-
-		//Fills out the name and recipe fields of each element based on eRecipes.csv
-		string text = textAsset.ToString ();
-		string[] array = text.Split('*');
-		List<Element> componentsAsElements = new List<Element> ();
-		foreach (string str in array) {
-			strList = str.Split(',').ToList();
-			components = strList.GetRange (1, strList.Count - 1);
-			componentsAsElements.Clear ();
-			foreach (string comp in components) {
-				mono.print (comp);
-				componentsAsElements.Add ((Element)comp);
-				mono.print ("Comp as Element: " + (Element)comp);
-			}
-			foreach (Element e in elements) {
-				if (e.name.Equals (strList [0])) {
-					e.components = componentsAsElements.ToArray();
-
-					string recipe = GiveRecipe (e.components);
-					if (recipe.Contains ("S")) {
-						sortingList = recipe.ToList ();
-						sortingList.Sort ();
-						stringList.Clear ();
-						foreach (char c in sortingList) {
-							stringList.Add (c.ToString ());
-						}
-						e.recipe = string.Join ("", stringList.ToArray ());
-					} else {
-						e.recipe = recipe;
-					}
-					break;
-				}
-			}
-		}
-
-		//Needed for next step, precalculated here. S = Aether.
-		string[] allPermutationsWORep = new string[] {"S", "A", "E", "F", "W",
-			"SA", "SE", "SF", "SW", "AS", "AE", "AF", "AW", "ES", "EA", "EF",
-			"EW", "FS", "FA", "FE", "FW", "WS", "WA", "WE", "WF", "SAE", "SAF",
-			"SAW", "SEA", "SEF", "SEW", "SFA", "SFE", "SFW", "SWA", "SWE", "SWF",
-			"ASE", "ASF", "ASW", "AES", "AEF", "AEW", "AFS", "AFE", "AFW", "AWS",
-			"AWE", "AWF", "ESA", "ESF", "ESW", "EAS", "EAF", "EAW", "EFS", "EFA",
-			"EFW", "EWS", "EWA", "EWF", "FSA", "FSE", "FSW", "FAS", "FAE", "FAW",
-			"FES", "FEA", "FEW", "FWS", "FWA", "FWE", "WSA", "WSE", "WSF", "WAS",
-			"WAE", "WAF", "WES", "WEA", "WEF", "WFS", "WFA", "WFE", "SAEF", "SAEW",
-			"SAFE", "SAFW", "SAWE", "SAWF", "SEAF", "SEAW", "SEFA", "SEFW", "SEWA",
-			"SEWF", "SFAE", "SFAW", "SFEA", "SFEW", "SFWA", "SFWE", "SWAE", "SWAF",
-			"SWEA", "SWEF", "SWFA", "SWFE", "ASEF", "ASEW", "ASFE", "ASFW", "ASWE",
-			"ASWF", "AESF", "AESW", "AEFS", "AEFW", "AEWS", "AEWF", "AFSE", "AFSW",
-			"AFES", "AFEW", "AFWS", "AFWE", "AWSE", "AWSF", "AWES", "AWEF", "AWFS",
-			"AWFE", "ESAF", "ESAW", "ESFA", "ESFW", "ESWA", "ESWF", "EASF", "EASW",
-			"EAFS", "EAFW", "EAWS", "EAWF", "EFSA", "EFSW", "EFAS", "EFAW", "EFWS",
-			"EFWA", "EWSA", "EWSF", "EWAS", "EWAF", "EWFS", "EWFA", "FSAE", "FSAW",
-			"FSEA", "FSEW", "FSWA", "FSWE", "FASE", "FASW", "FAES", "FAEW", "FAWS",
-			"FAWE", "FESA", "FESW", "FEAS", "FEAW", "FEWS", "FEWA", "FWSA", "FWSE",
-			"FWAS", "FWAE", "FWES", "FWEA", "WSAE", "WSAF", "WSEA", "WSEF", "WSFA",
-			"WSFE", "WASE", "WASF", "WAES", "WAEF", "WAFS", "WAFE", "WESA", "WESF",
-			"WEAS", "WEAF", "WEFS", "WEFA", "WFSA", "WFSE", "WFAS", "WFAE", "WFES",
-			"WFEA", "SAEFW", "SAEWF", "SAFEW", "SAFWE", "SAWEF", "SAWFE", "SEAFW",
-			"SEAWF", "SEFAW", "SEFWA", "SEWAF", "SEWFA", "SFAEW", "SFAWE", "SFEAW",
-			"SFEWA", "SFWAE", "SFWEA", "SWAEF", "SWAFE", "SWEAF", "SWEFA", "SWFAE",
-			"SWFEA", "ASEFW", "ASEWF", "ASFEW", "ASFWE", "ASWEF", "ASWFE", "AESFW",
-			"AESWF", "AEFSW", "AEFWS", "AEWSF", "AEWFS", "AFSEW", "AFSWE", "AFESW",
-			"AFEWS", "AFWSE", "AFWES", "AWSEF", "AWSFE", "AWESF", "AWEFS", "AWFSE",
-			"AWFES", "ESAFW", "ESAWF", "ESFAW", "ESFWA", "ESWAF", "ESWFA", "EASFW",
-			"EASWF", "EAFSW", "EAFWS", "EAWSF", "EAWFS", "EFSAW", "EFSWA", "EFASW",
-			"EFAWS", "EFWSA", "EFWAS", "EWSAF", "EWSFA", "EWASF", "EWAFS", "EWFSA",
-			"EWFAS", "FSAEW", "FSAWE", "FSEAW", "FSEWA", "FSWAE", "FSWEA", "FASEW",
-			"FASWE", "FAESW", "FAEWS", "FAWSE", "FAWES", "FESAW", "FESWA", "FEASW",
-			"FEAWS", "FEWSA", "FEWAS", "FWSAE", "FWSEA", "FWASE", "FWAES", "FWESA",
-			"FWEAS", "WSAEF", "WSAFE", "WSEAF", "WSEFA", "WSFAE", "WSFEA", "WASEF",
-			"WASFE", "WAESF", "WAEFS", "WAFSE", "WAFES", "WESAF", "WESFA", "WEASF",
-			"WEAFS", "WEFSA", "WEFAS", "WFSAE", "WFSEA", "WFASE", "WFAES", "WFESA", "WFEAS" };
-
-		//Initializes Dictionary<string, Element> recipeDict
-		recipeDict = new Dictionary<string, Element> ();
-		string sortedString;
-		foreach (string r in allPermutationsWORep) { //Note: would like to turn it into a pickle at some point
-			Element target = null;
-			sortingList = r.ToList ();
-			sortingList.Sort ();
-			stringList.Clear();
-			foreach (char c in sortingList) {
-				stringList.Add (c.ToString());
-			}
-			sortedString = string.Join ("", stringList.ToArray());
-			foreach (Element e in elements) {
-				if (e.recipe.Equals (sortedString)) {
-					target = e;
-					break;
-				}
-			}
-			recipeDict.Add(r, target);
-		}
-
-	}
-
-	#region Utility shit
-	//what do I want? I want...
-	//One element object for each element, containing the string name, components as an Element[], and the recipe as a string
-	//since that is 1) useful for other initialization steps, and 2) maybe better than the components in some case
-	//as well as the ability to give a recipe and return the element result
-	//Translates to:
-	//Element.sound.name -> "Sound" (for image setting)
-	//Element.sound.components -> [Element.aether, Element.air] (for reversing transmutation & checking if valid for mon)
-	//Element.aether + Element.air -> Element.sound (for combining elements) //adding x to x enhances to a point (eventually)
-	//adding components can't work, but needs code response, i.e. can't return null or the superset element
-	//Element.sound - Element.air -> Element.sound (for combining elements) //don't actually think we need this
-
-	public override string ToString()	{ return name; }
+	public static implicit operator string(Element e){ return e.name; }
 
 	public static explicit operator Element(string str){
-		//TODO: Turn elements into a HashSet for faster casting
 		for (int i = 0; i < elementStrings.Count; i++){
-			mono.print ("Str: " + str);
-			mono.print ("ElementStrings[i]" + elementStrings [i]);
-			mono.print ("Elements[i]" + elements [i]);
 			if (string.Equals(str, elementStrings[i], System.StringComparison.OrdinalIgnoreCase)){
-//				mono.print ("Str: " + str);
-//				mono.print ("ElementStrings[i]" + elementStrings [i]);
-//				mono.print ("Elements[i]" + elements [i]);
 				return elements[i];
 			}
 		}
 		UnityEngine.Debug.Log ("Error: \"" + str + "\" is not the name of an element, Origin: Element cast from string");
-		return null; //Doesn't this possibility mean that this cast should be explicit?
-	}
-
-	public static implicit operator string(Element e){ return e.name; }
-
-	public int CompareTo(Element that){
-		return this.name.CompareTo (that.name);
+		return null;
 	}
 
 	public static Element operator +(Element former, Element latter)
 	{
-		int comparison = former.CompareTo (latter);
-		if (comparison == 0) {
-			return former;
-		}
-
-		IEnumerable<char> iec = (former.recipe + latter.recipe).Distinct ();
-		mono.print (former.recipe);
-		List<string> stringList = new List<string>();
-		foreach (char c in iec) {
-			stringList.Add(c.ToString());
-			mono.print (c.ToString ());
-		}
-		string r = string.Join ("", stringList.ToArray());
-		mono.print ("recipe: " + r);
-		return recipeDict [r];
-
+		return Transmute (new Element[] { former, latter });
 	}
-
-//	private static string DecideLetter(Element e, List<Element> l, string s){
-//		if (l.Contains (e)) {
-//			return s + e.name [0];
-//		}
-//		DecideLetter(e
-//	}
-
-	private static string GiveRecipe(Element[] elementArray){
-		string recipe = "";
-		foreach (Element e in elementArray) {
-//			mono.print ("GiveRecipe for " + e.ToString ());
-//			mono.print ("Component1" + e.components[0].ToString ());
-			foreach (Element c in e.components) {
-				if (c == aether) {
-					recipe += "S";
-				} else {
-					recipe += c.name [0];
+		
+	private static void Init(TextAsset textAsset){
+		
+		//Fills out the components member of each element based on eRecipes.csv
+		List<string> strList;
+		List<string> components;
+		string text = textAsset.ToString ();
+		string[] array = text.Split('*');
+		List<Element> componentsAsElements = new List<Element> ();
+		foreach (string str in array) {
+			strList = str.Split (',').ToList ();
+			components = strList.GetRange (1, strList.Count - 1);
+			componentsAsElements.Clear ();
+			foreach (string comp in components) {
+				componentsAsElements.Add ((Element)comp);
+			}
+			foreach (Element e in elements) {
+				if (e.name.Equals (strList [0])) {
+					e.components = componentsAsElements.ToArray ();
 				}
 			}
 		}
-		return new string (recipe.Distinct ().ToArray ());
-	}
 
-	public static Element Transmute(Element[] elementArray){
-		string recipe = GiveRecipe (elementArray);
-		IEnumerable<char> iec = recipe.Distinct ();
-		List<string> stringList = new List<string>();
-		foreach (char c in iec) {
-			stringList.Add(c.ToString());
+		//Using 1 through n powers of n, where n is the max length of any combination of elements
+		//(i.e. 5 elements in sidereal = Aether + Air + Earth + Fire + Water),
+		//a system of ints where any combination results in a unique sum is achieved.
+		//This allows adding elements together by adding the "recipe" values of their components,
+		//as implemented in Transmute(Element[] elementArray).
+		aether.recipe	= 5;
+		air.recipe		= 25;
+		earth.recipe	= 125;
+		fire.recipe		= 625;
+		water.recipe	= 3125;
+
+		recipeDict = new Dictionary<int, Element>();
+		int total = 0;
+		foreach (Element e in elements) {
+			if (e.recipe == 0) {
+				foreach (Element c in e.components) {
+					total += c.recipe;
+				}
+				e.recipe = total;
+			}
+			recipeDict.Add (e.recipe, e);
+			total = 0;
 		}
-		recipe = string.Join ("", stringList.ToArray());
-		return recipeDict [recipe];
+
+	}
+		
+	public static Element Transmute(Element[] elementArray){
+		HashSet<int> componentrecipes = new HashSet<int>();
+
+		foreach (Element e in elementArray) {
+			foreach (Element c in e.components) {
+				componentrecipes.Add (c.recipe);
+			}
+		}
+
+		return recipeDict [componentrecipes.Sum()];
 	}
 
-	/// <summary>
-	/// Convenience method for dev purposes. Faster, but requires you to do some work.
-	/// Don't use for production purposes.
-	/// Instructions: For each element, consider its components, e.g. dragon = air + earth + fire.
-	/// Take the first letter of each, capitalize it, and turn it into a string (dragon = AEF)
-	/// Order does not matter and Aether is "S" (e.g. sound = "SA" or "AS", both work).
-	/// Finally, remove any duplicates.
-	/// Example: steam + basilisk + air -> F + W + A + E + S + A -> FWAESA -> FWAES = Sidereal...
-	/// Summary: Take first letter of name for each component, Aether is "S", no dupe characters, order not important
-	/// </summary>
-	public static Element Transmute(string recipe){
-		return recipeDict [recipe]; 
-	}
-
-	//thoughts
-	//so each element must have a field of its recipe, no? So that when combining say sound + dark
-	//it goes foreach element in sound.components, give recipe, then add those to dark's, then remove duplicates
-	//then plug into dictionary. Sorting not required, I've decided to just have more keys, since the number of values
-	//in memory is still the same. A hundred or more extra strings and their connections in the dictionary don't seem
-	//more valuable than the processing time of sorting over and over again many dozens of times. But that's just me.
-	//UPDATE: Learned what sorting strings is like in c#. Yes. Definitely. Once all the element names are finalized,
-	//pickle the damn dictionary, remove calls to Init, keep the function just in case, and hope that's it. It's just
-	//too bad that I still have to do the distinct character nonsense in GiveRecipe, but what can I do? If there's a
-	//better, faster, and cleaner looking way to do this that doesn't involve strings, I'm all for it. But right now,
-	//everything I've found is way above my paygrade. I don't have to time to learn it, unfortunately, so hopefully one
-	//day I'll know more.
-
+	public override string ToString()	{ return name; }
 	#endregion
+
 }
